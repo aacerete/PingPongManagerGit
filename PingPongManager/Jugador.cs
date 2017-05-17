c215d02d5bc52021a3f1e27ab638ec32bba2a5e8
aacerete <46990527d@iespoblenou.org>
2017-05-17 20:11:42 +0200

    Test unitarios

--- PingPongManager/Jugador.cs
+++ PingPongManager/Jugador.cs
@@ -7,7 +7,7 @@ using System.Windows.Forms;
 
 namespace PingPongManager
 {
-    class Jugador
+    public class Jugador
     {
         public String Id { get; set; }
         public String Nombre { get; set; }
@@ -15,7 +15,7 @@ namespace PingPongManager
         public int Jugados { get; set; }
         public int Puntos { get; set; }
 
-        public Jugador(String nombre, String image)
+        public Jugador(String nombre, String image = null)
         {
             Nombre = nombre;
             Image = image;
@@ -24,6 +24,41 @@ namespace PingPongManager
         public override string ToString()
         {
             return Nombre + " - " + Id;
+        }
+
+        public int getPuntos()
+        {
+            return this.Puntos;
+        }
+
+        public int getJugados()
+        {
+            return this.Jugados;
+        }
+
+        public int getPartidosPorDos()
+        {
+            int multiplicador = 2;
+            int resultado = multiplicador * this.Jugados;
+
+            return resultado;
+        }
+
+        public int getPuntuacion()
+        {
+            int puntuacion;
+
+            return puntuacion = this.Jugados * 3;
+        }
+
+        public void setPuntos(int puntos)
+        {
+            this.Puntos = puntos;
+        }
+
+        public void setJugados(int jugados)
+        {
+            this.Jugados = jugados;
         }
     }
 }
---
+++ PingPongManagerTests/JugadorTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PingPongManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongManager.Tests
+{
+    [TestClass()]
+    public class JugadorTests
+    {
+        [TestMethod()]
+        public void JugadorTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Jugador player = new Jugador("1", null);
+            Assert.IsNotNull(player.ToString());
+        }
+
+        [TestMethod()]
+        public void ToStringTestNotNull()
+        {
+            Jugador player = new Jugador("Player 1");
+            Assert.IsTrue(player.ToString() != null);
+
+        }
+
+        [TestMethod()]
+        public void getPuntosTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getJugadosTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getPartidosPorDosTest()
+        {
+            Jugador player = new Jugador("1", null);
+            player.setJugados(2);
+
+            Assert.IsTrue(player.getJugados() > 0);
+        }
+
+        [TestMethod()]
+        public void getPuntuacionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void setPuntosTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void setJugadosTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
